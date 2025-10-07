7cc1fd135b8718906bb2a9f857a91d98d1f91548
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-10-07 12:29:33 +0200

    Refactor

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -20,8 +20,10 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Molitor\Currency\Filament\Resources\CurrencyResource;
 use Molitor\Language\Filament\Resources\LanguageResource;
+use Molitor\Product\Filament\Resources\ProductCategoryResource;
 use Molitor\Product\Filament\Resources\ProductFieldOptionResource;
 use Molitor\Product\Filament\Resources\ProductFieldResource;
+use Molitor\Product\Filament\Resources\ProductResource;
 use Molitor\Product\Filament\Resources\ProductUnitResource;
 
 class AdminPanelProvider extends PanelProvider
@@ -43,6 +45,8 @@ class AdminPanelProvider extends PanelProvider
                 ProductUnitResource::class,
                 ProductFieldResource::class,
                 ProductFieldOptionResource::class,
+                ProductResource::class,
+                ProductCategoryResource::class,
             ])
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
