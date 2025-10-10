005e883d9b4a6383d0e9f4405a3bcb13a2a8a4f8
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-10-10 12:36:29 +0200

    namespace

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -20,11 +20,13 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Molitor\Currency\Filament\Resources\CurrencyResource;
 use Molitor\Language\Filament\Resources\LanguageResource;
+use Molitor\Product\Filament\Resources\BarcodeResource;
 use Molitor\Product\Filament\Resources\ProductCategoryResource;
 use Molitor\Product\Filament\Resources\ProductFieldOptionResource;
 use Molitor\Product\Filament\Resources\ProductFieldResource;
 use Molitor\Product\Filament\Resources\ProductResource;
 use Molitor\Product\Filament\Resources\ProductUnitResource;
+use Molitor\Unas\Filament\Resources\UnasProductResource;
 use Molitor\Unas\Filament\Resources\UnasShopResource;
 use Molitor\Customer\Filament\Resources\CustomerGroupResource;
 use Molitor\Customer\Filament\Resources\CustomerResource;
@@ -62,7 +64,7 @@ class AdminPanelProvider extends PanelProvider
                 WarehouseResource::class,
                 WarehouseRegionResource::class,
                 UnasShopResource::class,
-                \Molitor\Unas\Filament\Resources\UnasProductResource::class,
+                UnasProductResource::class,
             ])
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
